af7ffdf136d6d46c4e88aa4ee993277486626217
D. Pranav <132156340+diamondbucket@users.noreply.github.com>
2024-07-16 14:06:10 +0530

    animations

--- startup_dir/src/components/dotted_navbar.jsx
+++ startup_dir/src/components/dotted_navbar.jsx
@@ -1,49 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-scroll";
 
 const dotted_navbar = () => {
   return (
-    <ul className="py-2 px-7 rounded-full flex space-x-32 border-2 border-dotted overflow-hidden border-purple-600">
-      <li className="relative group z-20  ">
-        <Link
-          to="home"
-          spy={true}
-          smooth={true}
-          offset={50}
-          duration={500}
-          className="text-white cursor-pointer"
-        >
-          Product
-        </Link>
-        {/* <div className="absolute bottom-0 left-0 w-full h-0.5 bg-gradient-to-r from-transparent via-white to-transparent group-hover:bg-current"></div> */}
+    <ul className="relative py-4 px-10 rounded-full flex space-x-32 border-2 border-dotted overflow-hidden border-purple-600">
+      <li className="relative group z-20 text-white cursor-pointer">
+        <FlyoutLink>Product</FlyoutLink>
       </li>
-      <li className="relative group z-20 ">
-        <Link
-          to="about"
-          spy={true}
-          smooth={true}
-          offset={50}
-          duration={500}
-          className="text-white cursor-pointer"
-        >
-          Resources
-        </Link>
-        {/* <div className="absolute bottom-0 left-0 w-full h-0.5 bg-gradient-to-r from-transparent via-white to-transparent"></div>*/}
+      <li className="relative group z-20 text-white cursor-pointer">
+        <FlyoutLink>Resources</FlyoutLink>
       </li>
-      <li className="relative group z-20">
-        <Link
-          to="services"
-          spy={true}
-          smooth={true}
-          offset={50}
-          duration={500}
-          className="text-white cursor-pointer"
-        >
-          Community
-        </Link>
-        {/*<div className="absolute bottom-0 left-0 w-full h-0.5 bg-gradient-to-r from-transparent via-white to-transparent group-hover:bg-current"></div> */}
+      <li className="relative group z-20 text-white cursor-pointer">
+        <FlyoutLink>Community</FlyoutLink>
       </li>
     </ul>
+  );
+};
+
+const FlyoutLink = ({ children, href, FlyoutContent }) => {
+  const [open, SetOpen] = useState(false);
+  return (
+    <div
+      onMouseEnter={() => SetOpen(true)}
+      onMouseLeave={() => SetOpen(false)}
+      className="relative w-fit h-fit"
+    >
+      <a href={href} className="relative text-white">
+        {children}
+        <span
+          style={{
+            transform: open ? "scaleX(1)" : "scaleX(0)",
+          }}
+          className="absolute -bottom-2 -left-2 -right-2 h-1 origin-center rounded-full bg-purple-500 transition-transform duration-300 ease-out"
+        />
+      </a>
+    </div>
   );
 };
 
